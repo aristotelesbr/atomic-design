2d3bd11fb9c0be3975b96d1a7c20d12da1810b3a
aristotelesbr <aristotelesbr@gmail.com>
2021-07-11 10:23:31 -0300

    feat: added Container compoent

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="container">
-    <Services />
+    <Container>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione deserunt
+      at reiciendis labore quaerat ipsam excepturi repellendus animi numquam
+      quidem, placeat cum quas dolores architecto et nam vero, aut eveniet!
+    </Container>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { Services } from './components/organisms'
+import { Container } from './components/bosons'
 
 export default defineComponent({
-  components: { Services }
+  components: { Container }
 })
 </script>
 
